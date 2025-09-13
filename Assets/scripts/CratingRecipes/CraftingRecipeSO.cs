76cce93b4cba6187ada4aa26f47cc6b0e8737090
HellFuryCode <hannah.condon.za@gmail.com>
2025-09-13 15:41:37 +0200

    updates to crafting

--- Assets/scripts/CratingRecipes/CraftingRecipeSO.cs
+++ Assets/scripts/CratingRecipes/CraftingRecipeSO.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-[CreateAssetMenu()]
+[CreateAssetMenu(menuName = "Crafting/Recipe", fileName = "Recipe_")]
 public class CraftingRecipeSO : ScriptableObject
 {
     public Sprite sprite;
---
+++ Assets/scripts/UI/CharacterSelctUI.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CharacterSelctUI : MonoBehaviour
+{
+    [SerializeField] private Button mainMenuButton;
+    [SerializeField] private Button readyButton;
+
+
+  
+}
