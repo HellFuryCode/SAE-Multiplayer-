ec1e96dfbc07e322bcc8add6a01a43f367715b1c
HellFuryCode <hannah.condon.za@gmail.com>
2025-09-15 09:37:40 +0200

    Scene Mangaemnet

--- Assets/scripts/UI/CharacterSelctUI.cs
+++ Assets/scripts/UI/CharacterSelctUI.cs
@@ -1,12 +1,39 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class CharacterSelctUI : MonoBehaviour
 {
-    [SerializeField] private Button mainMenuButton;
-    [SerializeField] private Button readyButton;
+    public GameObject[] characters;
+    public int selectedCharcter = 0;
 
+    public void NextCharacter()
+    {
+        characters[selectedCharcter].SetActive(false);
+        selectedCharcter = (selectedCharcter + 1) % characters.Length;
+        characters[selectedCharcter].SetActive(true);
+    }
 
-  
+    public void PreviousCharacter()
+    {
+        characters[selectedCharcter].SetActive(false);
+        selectedCharcter--;
+        if (selectedCharcter < 0)
+        {
+            selectedCharcter += characters.Length;
+        }
+        characters[selectedCharcter].SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        PlayerPrefs.SetInt("selectedCharcter", selectedCharcter);
+        SceneManager.LoadScene(1);
+    }
+
 }
+
+
+
+//https://www.youtube.com/watch?v=7glCsF9fv3s&list=PLzDRvYVwl53sSmEcIgZyDzrc0Smpq_9fN
+//https://www.youtube.com/watch?v=3qlRgICRoeA
